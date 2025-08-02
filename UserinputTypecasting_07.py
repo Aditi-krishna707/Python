bcb9a86be597aad2c25ecc7e4b5ef82193634780
Aditi <aditi.gla@gla.ac.in>
2025-08-02 17:25:54 +0530

    input

---
+++ UserinputTypecasting_07.py
@@ -0,0 +1,18 @@
+# input is aleways captured as string 
+# so we use typecasting->converting one datatype to another
+
+# name=input("Entre your name :")
+# print(name)
+
+
+# roll=input("Entre your roll :")
+# print(roll)
+# print(type(roll))
+
+
+# roll=int(input("Entre your roll :"))
+# print(roll)
+# print(type(roll))
+
+
+
