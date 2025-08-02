72f340b06a69499237da33f5d92ec4ab6732ca06
Aditi <aditi.gla@gla.ac.in>
2025-08-02 17:15:34 +0530

    ord() chr()

---
+++ datatypeASCII_06.py
@@ -0,0 +1,16 @@
+# Numeric(int float,complex number),dictinary{key:value pair},boolean,sequence type(string ,list[],tuple(can't modify)),set({unique})
+
+# American Standards code for information interchange
+# ASCII->represents charracter as numeric code
+"""
+A-Z ->65-90
+a-z ->97-122
+'0'->48
+space->32
+
+"""
+# ASCII value of any number iss find by function ord()
+# print(ord('a'))
+
+# chr()->char for the ASCII value
+print(chr(97))
