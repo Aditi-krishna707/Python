78c526babc96ae1d5e3b58fcf89943193c8c4276
Aditi <aditi.gla@gla.ac.in>
2025-08-03 20:04:42 +0530

    loops

---
+++ loop_12.py
@@ -0,0 +1,14 @@
+# loop->allow to control the flow of program
+
+# raining=True
+# if(raining):
+#    print("yes")
+# else:
+#    print("Not raining")
+
+number=int(input("entre number"))
+if(number>0):
+  print("+")
+else:
+  print("!+") 
+print("-ve")   
