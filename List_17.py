eb086798d4b8788bfdcdf89e7c6f5f53796cfa31
Aditi <aditi.gla@gla.ac.in>
2025-08-09 22:50:20 +0530

    python

---
+++ List_17.py
@@ -0,0 +1,138 @@
+# what is list?
+# it allow you to store multiple itemsin a single variable
+"""
+1.indexed
+2.ordered
+3.mutable
+4.list can be made of different datatpe
+5.a single list can contain different datatype
+6.Duplicatesallowed
+"""
+
+# a=["Aditi","nimi","jyoti","jay",12.12.5,false]
+a=["Aditi","nimi","jyoti","jay"]
+# print all list
+# print(a)
+
+# checktype of list
+# print(type(a))
+
+# find the length of list
+# print(len(a))
+
+# check if item is in list
+# if "nimi" 
+
+# if a item is not present in the list
+# if "tanamy" not in a:
+#     print("tanmay is not in this list")
+
+
+# Accessing in list 
+"""
+1.indexing
+2.range of index(inclusive,exclusive)
+3.renage of negative index(-1 se start hoga aur piche se)
+4Negative indexing"""
+
+# names=["tanmay","ashu","ankur","ankus","mayank","krishna"]
+# print(names[0])
+# print(names[-6])
+# print(names[1:3])
+# print(names[-3:-1])
+
+
+# adding element to the list
+"""
+append()->adding item to the end of the list
+insert()->it add element at the given index
+extend()->when we extend 2 list then the the element of second list come after list 
+"""
+
+# names=["tanmay","ashu","ankur","ankus","mayank","krishna"]
+# names2=["Aditi","Anushka","priyal","Kratika"]
+# names.append("Sohil khan")
+# print(names)
+
+# names.insert(2,"TST")
+# print(names)
+
+# names.extend(names2)
+# print(names)
+
+
+# Remove element from the list    
+# remove()->remove specified item
+# pop()->it remove item of the give index ot the last elment
+
+names=["tanmay","ashu","ankur","ankus","mayank","krishna"]
+# names.remove("tanmay")
+# print(names)
+
+# remove the last element
+# names.pop()
+# print(names)
+
+# remove element which is at the index 1
+# names.pop(1)
+# print(names)
+
+# changing item in the list
+# at the index
+# in the range
+
+
+marks=[00,10,20,30,40,50,60,70]
+# marks[1]=100
+# print(marks)
+
+# marks[1:4]=[400]
+# SEE CAREFULLY
+# print(marks)
+
+
+
+# Sorting a list
+# nums=[2,5,6,3,33,2,5,68,8]
+# nums.sort()
+# print(nums)
+# nums.sort(reverse=True)
+# print(nums)
+
+# nums.reverse()
+# print(nums)
+
+
+# LIST COMPREHENSION->when we want to make new list from items of the existing list
+list=[20,8,5,444,99,20,70,23]
+newlist=[]
+# for i in list:
+#     if i>25:
+#         newlist.append(i)
+# print(newlist)
+# 
+# OR
+# newlist=[list for lists in list if list > 25 in list] 
+# print(newlist)       
+
+
+# lst = [20, 8, 5, 444, 99, 20, 70, 23]  
+# newlist = [x for x in lst if x > 25]
+# print(newlist)
+
+
+# collage=["GLA","SRM","IITB","IITD"]
+# newlist=collage.copy()
+# print(newlist)
+
+# name1=["tanmay","Ajay","Jay","Akash"]
+# name2=["Aditi","Anshu","priyal","Akash"]
+# combine=name1+name2
+# print(combine)
+
+
+# Nested list
+num1=[1,2,3,99,100,45]
+num1.insert(2,[55,66,77])
+print(num1)
+print(num1[2][1])
---
+++ Pattern_16.py
@@ -0,0 +1,30 @@
+"""
+rows 
+column
+what to print
+"""
+# n=int(input("Entre colm number "))
+# for _ in range(n):
+#     print("*"*5)
+
+
+
+# 1 2 3 
+# 1 2 3
+# 1 2 3
+# n=int(input("Entre no of row :"))
+# for i in range(1,n+1):
+#     for j in range(1,n+1,):
+#         print(j,end=" ")
+#     print()    
+
+
+# 1
+# 1 2
+# 1 2 3
+i=int(input("Entre row no"))
+for i in range(i,i+1):
+    for j in range(1,i):
+        print(j)
+    print()    
+
