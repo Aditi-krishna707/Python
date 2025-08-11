1fcb17b5177964f0ee546e9f5533d99086959b39
Aditi <aditi.gla@gla.ac.in>
2025-08-11 09:40:25 +0530

    python

---
+++ Function_22.py
@@ -0,0 +1,43 @@
+# block of reusable code that perform specific task
+# types of function
+# user defined
+# build in
+
+
+# print a ffunction that print hrllo world
+
+"""def Hel():
+    print("Hello world")
+Hel()    
+Hel()    
+Hel()    
+Hel()  """  
+
+
+# types of Argument
+# 1.Default Argument
+# 2.keywoed argument(named argement)
+# 3.Positional argument
+# 4.Arbitrary (variable length arguments)
+
+# parameter
+# def sum(n1,n2):
+#     sum=n1+n2
+#     return sum
+# #Positional argument
+# print(sum(2,3))
+
+
+# def sum(n1,n2):
+#     sum=n1+n2
+#     return sum
+# #named argument
+# print(sum(n1=2,n2=3))
+
+
+def sum(n1,n2=0):
+    sum=n1+n2
+    return sum
+#default argument
+print(sum(2))
+
