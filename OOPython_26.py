a2de2b7a57fd1856b66dad07e4c36ab2c52c0926
Aditi <aditi.gla@gla.ac.in>
2025-08-15 03:00:56 +0530

    python

---
+++ OOPython_26.py
@@ -0,0 +1,27 @@
+"""There are several paradigms in programming:
+
+Procedural programming – focuses on writing procedures/functions (step-by-step instructions).
+
+Functional programming – focuses on pure functions and immutability.
+
+Object-oriented programming – focuses on objects that combine data and behavior."""
+# Object oriented programming
+# it is a programming paradigm where the software design revolves around object/data rather than function
+
+
+# why we use
+"""
+1.helpto memmic real worls entities and thrie interection
+2.code reusability
+3.organisation and maintability of code"""
+
+"""classes and object"""
+# classes->user defined datatype
+# classes are blueprint/templet for a object 
+
+
+# object are the instance of type class
+# classed memic real world entities
+
+# varible->attributes
+# function->method
--- String_25.py
+++ String_25.py
@@ -151,5 +151,12 @@ friend="priyal"
 # like \' , \\,\t,\r,\b
 # backslace(\)
 
+# check palindrome
 
+# def check_palindrome(str):
+#     clear_str=(str.replace(" ","")).lower()
+#     reversed_str=clear_str[::-1]
+#     return reversed_str==clear_str
+# str=input("Entre string  ")
+# print(check_palindrome(str))
 
